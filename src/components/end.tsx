4fa87d488a7015e961574905025f232960bebf32
PavelBokun <pavel.snabteh@gmail.com>
2024-08-12 22:32:04 +0300

    1208/3

--- src/components/end.tsx
+++ src/components/end.tsx
@@ -1,5 +1,5 @@
 export const End=()=>{
     return (
-        <div><h1>Радиаторы  в наличии на складе.<div className=""></div></h1></div>
+        <div><h1 className="changeColor2">Радиаторы  в наличии на складе.<div className=""></div></h1></div>
     )
 }
